8cfb4e039543c9fae45716181e70d335ddc558de
Justin <info@justinle.me>
2022-06-28 16:01:09 -0700

    using cache to display pokemon

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,18 +1,32 @@
 import { useState, useEffect } from 'react';
-import { getPokemon } from '../utils/getPokemon';
+import { getPokemon, getCachedPokemon } from '../utils/request';
 
 interface Pokemon {
     name: string;
     url: string;
 }
 
-export default function Cards() {
+export default function Cards({ useCache }: { useCache: boolean }) {
     const [pokemon, setPokemon] = useState<Array<Object>>([]);
 
     useEffect(() => {
-        getPokemon().then((data) => {
-            setPokemon(data);
-        });
+        const callApi = async () => {
+            setPokemon([]);
+
+            if (useCache) {
+                const cachedPokemon = getCachedPokemon();
+
+                if (cachedPokemon) {
+                    setPokemon(cachedPokemon);
+                }
+            } else {
+                const callPokemon = await getPokemon(useCache);
+                setPokemon(callPokemon);
+            }
+        };
+
+        callApi();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     console.log(pokemon[`results`]);
@@ -22,7 +36,9 @@ export default function Cards() {
             <div className="grid grid-cols-5 gap-4">
                 {pokemon[`results`].map((pokemon: Pokemon, index: number) => (
                     <div className="rounded shadow-md" key={index}>
-                        {pokemon.name}
+                        {(index + 1).toString().padStart(3, '0')}{' '}
+                        {pokemon.name.charAt(0).toUpperCase() +
+                            pokemon.name.slice(1)}
                     </div>
                 ))}
             </div>
