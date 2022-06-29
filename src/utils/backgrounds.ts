a2bca93622da84b3e99a39764d4e371ca5a39da8
Justin <info@justinle.me>
2022-06-28 19:43:33 -0700

    background color utility

---
+++ src/utils/backgrounds.ts
@@ -0,0 +1,31 @@
+const typeColor = {
+    rock: `#BBAA66`,
+    ghost: `#6666BA`,
+    steel: `#AAAABB`,
+    water: `#3399FE`,
+    grass: `#76CC55`,
+    psychic: `#FF5599`,
+    ice: `#65CCFF`,
+    dark: `#775444`,
+    fairy: `#EE99EE`,
+    normal: `#AAAA9B`,
+    fighting: `#BA5544`,
+    flying: `#8799FF`,
+    poison: `#AA5599`,
+    ground: `#DDBB54`,
+    bug: `#A9BB22`,
+    fire: `#EB5435`,
+    electric: `#FFCC33`,
+    dragon: `#6666BA`
+};
+
+const otherColors = {
+    DarkGray: `#333333`,
+    MediumGray: `#828282`,
+    LightGray: `#E0E0E0`,
+    White: `#FFFFFF`,
+    Background: `#FAFAFA`,
+    PokemonRed: `#EB5435`
+};
+
+export { typeColor, otherColors };
