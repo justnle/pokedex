3322c3012475ff4c01611aac4a306c5a4f64d3dc
Justin <info@justinle.me>
2022-06-30 15:52:46 -0700

    moved Capture to page

--- src/components/Captured.tsx
+++
@@ -1,3 +0,0 @@
-export default function Captured() {
-    return <div></div>;
-}
---
+++ src/pages/Captured.tsx
@@ -0,0 +1,9 @@
+import Header from '../components/Header';
+
+export default function Captured() {
+    return (
+        <div className="captured">
+            <Header />
+        </div>
+    );
+}
