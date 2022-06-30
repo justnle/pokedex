fc0c2f5264c247a6bd8444f6c31e632eacc6545f
Justin <info@justinle.me>
2022-06-30 15:53:26 -0700

    renders pages on click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import pokedexLogo from '../assets/pokedex_logo.png';
 import pokeball from '../assets/pokeball.png';
+import { Link } from 'react-router-dom';
 
 interface Style {
     [key: string]: string;
@@ -21,20 +22,26 @@ export default function Header() {
             className="header flex flex-row justify-between"
             style={headerStyle}
         >
-            <div className="logo">
-                <img src={pokedexLogo} alt="pokemon-logo" />
-            </div>
-            <div className="captured-button flex flex-row h-[72px] w-[420px] my-auto rounded-full bg-pokemon-red">
-                <div className="pokeball">
-                    <img
-                        src={pokeball}
-                        alt="pokeball"
-                        className="h-[72px] w-[72px]"
-                    />
+            <Link to="/">
+                <div className="logo">
+                    <img src={pokedexLogo} alt="pokemon-logo" />
                 </div>
-                <div className="captured-text text-white text-[36px] font-[590] my-auto">
-                    <span>Captured Pokemons</span>
-                </div>
+            </Link>
+            <div className="button-container my-auto h-[72px] w-[420px]">
+                <Link to="/captured">
+                    <div className="captured-button flex flex-row bg-pokemon-red rounded-full">
+                        <div className="pokeball">
+                            <img
+                                src={pokeball}
+                                alt="pokeball"
+                                className="h-[72px] w-[72px]"
+                            />
+                        </div>
+                        <div className="captured-text text-white text-[36px] font-[590] my-auto">
+                            <span>Captured Pokemons</span>
+                        </div>
+                    </div>
+                </Link>
             </div>
         </div>
     );
