573364feb89d2df37f9e08f2da59bbd30d8e64ae
Justin <info@justinle.me>
2022-06-30 13:53:13 -0700

    wrapped list in scroll component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
-import List from './components/List';
+import InfiniteScroll from './components/InfiniteScroll';
 import './App.css';
 
 function App() {
     return (
         <div className="App container mx-auto">
             <Header />
-            <List useCache={true} />
+            <InfiniteScroll useCache={true} />
         </div>
     );
 }
