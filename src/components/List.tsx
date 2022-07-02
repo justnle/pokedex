b1c361e45650d60eba921afb3beb1cf5ed665bf5
Justin <info@justinle.me>
2022-07-01 23:50:18 -0700

    adjusted styling, padding, etc

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import Card from './Card';
+import Detail from './Detail';
+import Header from './Header';
 
 export default function List(props: any) {
     const [pokemonList, setPokemonList] = useState<Array<Object>>([]);
@@ -10,21 +12,29 @@ export default function List(props: any) {
     }, [props.pokemonList]);
 
     return (
-        <div className="list">
-            <div className="grid grid-cols-4 gap-4">
-                {!pokemonList ? (
-                    <div>No Pokemon List to Render</div>
-                ) : (
-                    pokemonList.map((pokemon: any, index: number) => {
-                        return (
-                            <Card
-                                key={`${pokemon[`name`]}-${index}`}
-                                {...pokemon}
-                                useCache={false}
-                            />
-                        );
-                    })
-                )}
+        <div className="main-container">
+            <div className="list-container flex flex-row">
+                <div className="container-lg">
+                    <Header />
+                    <div className="card-container flex flex-row flex-wrap justify-center gap-6 xl:gap-10">
+                        {!pokemonList ? (
+                            <div>No Pokemon List to Render</div>
+                        ) : (
+                            pokemonList.map((pokemon: any, index: number) => {
+                                return (
+                                    <Card
+                                        key={`${pokemon[`name`]}-${index}`}
+                                        {...pokemon}
+                                        useCache={false}
+                                    />
+                                );
+                            })
+                        )}
+                    </div>
+                </div>
+                {/* <div className="detail-container">
+                    <Detail />
+                </div> */}
             </div>
         </div>
     );
