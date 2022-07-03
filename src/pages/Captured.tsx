91dee319d544c84f723b944eea2445be795916bf
Justin <info@justinle.me>
2022-07-03 00:44:13 -0700

    captured page shows pokemon in localstorage

--- src/pages/Captured.tsx
+++ src/pages/Captured.tsx
@@ -1,9 +1,51 @@
+import { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 export default function Captured() {
+    const [capturedPokemon, setCapturedPokemon] = useState([]);
+
+    useEffect(() => {
+        const capturedPokemonList = localStorage.getItem(`capturedPokemon`);
+
+        if (capturedPokemonList) {
+            setCapturedPokemon(JSON.parse(capturedPokemonList));
+        }
+    }, []);
+
     return (
-        <div className="captured">
+        <div className="captured px-10">
             <Header />
+            <div className="captured-list">
+                <table className="table-auto w-full">
+                    <thead className="bg-light-gray/30 text-[24px]">
+                        <tr>
+                            <th className="">POKEMON</th>
+                            <th className="">NICKNAME</th>
+                            <th className="">CAPTURED AT</th>
+                            <th className="">CAPTURED LEVEL</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {capturedPokemon.map(
+                            (pokemon: Object, index: number) => {
+                                return (
+                                    <tr
+                                        key={`${
+                                            pokemon[`name`]
+                                        }-table-row-${index}`}
+                                        className="text-[28px]"
+                                    >
+                                        <td>{pokemon[`name`]}</td>
+                                        <td>{pokemon[`nickname`]}</td>
+                                        <td>{pokemon[`captured_date`]}</td>
+                                        <td>{pokemon[`captured_level`]}</td>
+                                    </tr>
+                                );
+                            }
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
