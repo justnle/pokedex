39eb10063895d362cf157b32341dadddf0a440dc
Justin <info@justinle.me>
2022-07-02 20:14:53 -0700

    pokemon data saved to localstorage when captured

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,42 @@
 export default function Modal() {
+    const toISOStringWithTimezone = (date: Date) => {
+        const timeZoneOffset = -date.getTimezoneOffset();
+        const diff = timeZoneOffset >= 0 ? `+` : `-`;
+        const pad = (num: number) =>
+            `${Math.floor(Math.abs(num))}`.padStart(2, `0`);
+
+        return (
+            date.getFullYear() +
+            `-` +
+            pad(date.getMonth() + 1) +
+            `-` +
+            pad(date.getDate()) +
+            `T` +
+            pad(date.getHours()) +
+            `:` +
+            pad(date.getMinutes()) +
+            `:` +
+            pad(date.getSeconds()) +
+            diff +
+            pad(timeZoneOffset / 60) +
+            `:` +
+            pad(timeZoneOffset % 60)
+        );
+    };
+
+    const capturePokemon = () => {
+        const captureData = {
+            name: `bulbasaur`, //pokemon.name
+            nickname: `optionalString`,
+            captured_date: toISOStringWithTimezone(new Date()),
+            captured_level: Math.floor(Math.random() * 101),
+            pokemon_detail: ``
+        };
+
+        localStorage.setItem(`test`, JSON.stringify(captureData));
+        console.log(`captured pokemon!`);
+    };
+
     return (
         <div className="modal flex flex-col items-center justify-center h-screen">
             <div className="modal-contents w-[348px] px-[24px] py-[32px] bg-white rounded-[16px]">
@@ -23,6 +61,7 @@ export default function Modal() {
                         name="capture-date"
                         placeholder="Captured Date"
                         className="w-full rounded-md my-1 pl-2 py-1 text-[18px] border border-black"
+                        disabled
                     ></input>
                     <input
                         type="text"
@@ -30,9 +69,13 @@ export default function Modal() {
                         name="capture-level"
                         placeholder="Captured Level"
                         className="w-full rounded-md my-1 pl-2 py-1 text-[18px] border border-black"
+                        disabled
                     ></input>
                 </div>
-                <div className="capture-button bg-red-500 rounded-[100px] mx-auto text-white text-[18px] font-semibold">
+                <div
+                    className="capture-button bg-red-500 rounded-[100px] mx-auto text-white text-[18px] font-semibold"
+                    onClick={() => capturePokemon()}
+                >
                     <h2 className="py-3">Capture</h2>
                 </div>
             </div>
