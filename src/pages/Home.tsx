fcaa3f05fc44f4b321cc0e91ea82c110b19196b4
Justin <info@justinle.me>
2022-07-12 23:08:03 -0700

    changing how previous page works..

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,13 @@ const baseURL: string = `https://pokeapi.co/api/v2/pokemon?limit=30&offset=0`;
 export default function Home(): JSX.Element {
     const [pokemonList, setPokemonList] = useState<Array<Object>>([]);
     const [nextPokemonList, setNextPokemonList] = useState<Array<Object>>([]);
+    const [previousPokemonList, setPreviousPokemonList] = useState<
+        Array<Object>
+    >([]);
     const [showPrevious, setShowPrevious] = useState<Boolean>(false);
     const [homePage, setHomePage] = useState<Array<Object>>([]);
+
+    let pageCount = 0;
 
     useEffect(() => {
         const getData = async () => {
@@ -25,19 +30,33 @@ export default function Home(): JSX.Element {
 
     const updatePokemonList = async () => {
         const newPokemon = await getPokemonData(pokemonList[`next`]);
+        setPreviousPokemonList((previousList) => [
+            ...previousList,
+            pokemonList
+        ]);
         setNextPokemonList(newPokemon);
         setPokemonList(newPokemon);
         setShowPrevious(true);
     };
 
     const updatePreviousPokemonList = async () => {
-        const oldPokemon = await getPokemonData(pokemonList[`previous`]);
-        setPokemonList(oldPokemon);
+        // const oldPokemon = await getPokemonData(pokemonList[`previous`]);
+        // setPokemonList(oldPokemon);
 
-        if (!oldPokemon[`previous`]) {
+        // if (!oldPokemon[`previous`]) {
+        //     setShowPrevious(false);
+        // }
+        const oldPokemon = previousPokemonList[pageCount];
+        console.log(oldPokemon);
+        // setPokemonList(oldPokemon);
+
+        if (!previousPokemonList[`previous`]) {
             setShowPrevious(false);
         }
     };
+
+    console.log(previousPokemonList[pageCount]);
+    console.log(pageCount);
 
     return (
         <div className="home-container pb-10">
@@ -50,13 +69,23 @@ export default function Home(): JSX.Element {
             <div className="pagination-button-container flex justify-around text-center text-[28px]">
                 {showPrevious ? (
                     <div className="previous-button-container font-bold">
-                        <button onClick={() => updatePreviousPokemonList()}>
+                        <button
+                            onClick={() => {
+                                updatePreviousPokemonList();
+                                --pageCount;
+                            }}
+                        >
                             Previous Page
                         </button>
                     </div>
                 ) : null}
                 <div className="next-button-container font-bold">
-                    <button onClick={() => updatePokemonList()}>
+                    <button
+                        onClick={() => {
+                            updatePokemonList();
+                            ++pageCount;
+                        }}
+                    >
                         Next Page
                     </button>
                 </div>
